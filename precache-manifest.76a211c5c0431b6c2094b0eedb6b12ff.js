13868237058700de8c24d3919c8ccb4871c97c02
sandeep Swami <sandeepswami1910@gmail.com>
2020-02-29 23:03:55 +0530

    Updates

---
+++ precache-manifest.76a211c5c0431b6c2094b0eedb6b12ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82cbaed2a18ec7e1576f21ff6c235a32",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "70db2a51c354168768a5",
+    "url": "/monsters-rolodex/static/css/main.8d21674c.chunk.css"
+  },
+  {
+    "revision": "a4787d7e1433e07d3e1f",
+    "url": "/monsters-rolodex/static/js/2.c10b6e54.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.c10b6e54.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70db2a51c354168768a5",
+    "url": "/monsters-rolodex/static/js/main.2d1eda5c.chunk.js"
+  },
+  {
+    "revision": "096d767aa4fa4597015e",
+    "url": "/monsters-rolodex/static/js/runtime-main.1a2ed4b0.js"
+  }
+]);
